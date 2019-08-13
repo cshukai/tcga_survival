5e489f960dbfd91312da657ceded596a7e0ac081
Shu-Kai Chang <cshukai@gmail.com>
2019-08-13 18:33:23 -0500

    Update coad.r

--- coad.r
+++ coad.r
@@ -1,7 +1,16 @@
 library(RTCGA)
 library(RTCGA.clinical)
 
+#################################EDA#################################
+stromaIdx=colnames(COAD.clinical)[grep(x=colnames(COAD.clinical),pattern="stroma")] # find where stroma -related colnames are
+
+#################################STROMA#################################
+# normalization
+for(i in 1:nrow(COAD.clinical)){
+}
+# stroma percentage survival
 clin=survivalTCGA(COAD.clinical)
 
-colnames(COAD.clinical)[grep(x=colnames(COAD.clinical),pattern="stroma")] # find where stroma -related colnames are
+
+
 
