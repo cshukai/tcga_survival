c592daa2c8150b026288cae5f14cd40c86223adf
Shu-Kai Chang <cshukai@gmail.com>
2019-08-15 23:18:34 -0500

    Update coad.r

--- coad.r
+++ coad.r
@@ -3,7 +3,7 @@ library(RTCGA.clinical)
 library(survival)
 library(survminer)
 #################################STROMA#################################
- 
+
 stromaIdx=colnames(COAD.clinical)[grep(x=colnames(COAD.clinical),pattern="stroma")] # find where stroma -related colnames are
 
 stroma_avg=NULL
@@ -65,8 +65,45 @@ group =ifelse(sur_tum_str$st_ratio>median(sur_tum_str$st_ratio),'high','low')
 
 sfit =survfit(Surv(times, patient.vital_status)~group, data=sur_tum_str)
 summary(sfit)
+surv_pvalue(sfit)$pval
 ggsurvplot(sfit, conf.int=F, pval=TRUE)
 
+########################other image features  #################################
+features=c("percent_granulocyte_infiltration", "percent_inflam_infiltration", "percent_lymphocyte_infiltration", "percent_monocyte_infiltration", "percent_necrosis", "percent_neutrophil_infiltration")
 
+for(i in 1:length(features)){
+  interestingCols=colnames(COAD.clinical)[intersect(grep(x=colnames(COAD.clinical),pattern=features[i]),grep(x=colnames(COAD.clinical),pattern="slide"))]
+  avg=NULL
+  for(j in 1:nrow(COAD.clinical)){
+    reps=0
+    percents=0
+    this_avg=-1
+    for(k in 1:length(interestingCols)){
+      this_percent=COAD.clinical[j,interestingCols[k]]
+      if(!is.na(this_percent)){
+        percents=percents+as.numeric(this_percent)
+        reps=reps+1
+      }
+    }
+    if(reps>0){
+      this_avg=percents/reps
+    }
+    this_row=cbind(COAD.clinical[j,"patient.bcr_patient_barcode"],this_avg)
+    avg=rbind(avg,this_row)
+    
+  }
+  avg=avg[-which(avg[,2]=="-1"),]
+  
+  
+}
+#"patient.samples.sample-2.portions.portion.slides.slide.percent_necrosis"
+#"patient.samples.sample.portions.portion.slides.slide.percent_eosinophil_infiltration"[1]
+#[1]https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4848192/
+[220] "patient.samples.sample.portions.portion.slides.slide.percent_granulocyte_infiltration"
+[221] "patient.samples.sample.portions.portion.slides.slide.percent_inflam_infiltration"
+[222] "patient.samples.sample.portions.portion.slides.slide.percent_lymphocyte_infiltration"
+[223] "patient.samples.sample.portions.portion.slides.slide.percent_monocyte_infiltration"
+[224] "patient.samples.sample.portions.portion.slides.slide.percent_necrosis"
+[225] "patient.samples.sample.portions.portion.slides.slide.percent_neutrophil_infiltration"
 
 save.image("tumorstroma.RData")
